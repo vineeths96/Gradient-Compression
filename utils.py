f57822499ea3af7edde12723b4fbe3bd6ac312c6
vineeths96 <vinsree1996@gmail.com>
2020-10-03 17:15:18 +0530

    Add Time breakdown plot function

--- utils.py
+++ utils.py
@@ -1,12 +1,16 @@
 import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 
 def plot_loss_curves(log_path):
     plt.figure(figsize=[10, 7])
 
     experiments = os.listdir(log_path)
+    experiments.sort()
+
     for experiment in experiments:
         reducer = None
         quant_level = None
@@ -47,6 +51,8 @@ def plot_top1_accuracy_curves(log_path):
     plt.figure(figsize=[10, 7])
 
     experiments = os.listdir(log_path)
+    experiments.sort()
+
     for experiment in experiments:
         reducer = None
         quant_level = None
@@ -87,6 +93,8 @@ def plot_top5_accuracy_curves(log_path):
     plt.figure(figsize=[10, 7])
 
     experiments = os.listdir(log_path)
+    experiments.sort()
+
     for experiment in experiments:
         reducer = None
         quant_level = None
@@ -127,6 +135,8 @@ def plot_time_per_batch_curves(log_path):
     plt.figure(figsize=[10, 7])
 
     experiments = os.listdir(log_path)
+    experiments.sort()
+
     for experiment in experiments:
         reducer = None
         quant_level = None
@@ -168,8 +178,61 @@ def plot_time_per_batch_curves(log_path):
     plt.show()
 
 
+def plot_time_breakdown(log_path):
+    plt.figure(figsize=[10, 7])
+
+    time_labels = ['batch', 'batch.accumulate', 'batch.backward', 'batch.evaluate',
+                   'batch.forward', 'batch.reduce', 'batch.step', 'epoch_metrics.collect']
+
+    experiments = os.listdir(log_path)
+    experiments.sort()
+
+    events = np.arange(len(time_labels))
+    num_experiments = (len(experiments) - 1)
+    width = 0.15
+
+    for ind, experiment in enumerate(experiments):
+        reducer = None
+        quant_level = None
+        compression = None
+
+        with open(os.path.join(log_path, experiment, 'success.txt')) as file:
+            for line in file:
+                line = line.rstrip()
+                if line.startswith("reducer"):
+                    reducer = line.split(': ')[-1]
+
+                if line.startswith("quantization_level"):
+                    quant_level = line.split(': ')[-1]
+
+                if line.startswith("compression"):
+                    compression = line.split(': ')[-1]
+
+            if quant_level:
+                label = ' '.join([reducer, quant_level, 'bits'])
+            elif compression:
+                label = ' '.join([reducer, 'K:', compression])
+            else:
+                label = reducer
+
+        time_df = pd.read_json(os.path.join(log_path, experiment, 'timer_summary.json')).loc['average_duration']
+        time_values = time_df[time_labels].values
+
+        plt.bar(events + (ind - num_experiments / 2) * width, time_values, width, label=label)
+
+    plt.xticks(events, time_labels)
+    plt.xticks(rotation=90)
+    plt.ylabel("Average time")
+    plt.title("Time breakdown")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("./plots/time_breakdown.png")
+    plt.show()
+
+
 if __name__ == '__main__':
     plot_loss_curves('./logs/plot_logs')
     plot_top1_accuracy_curves('./logs/plot_logs')
     plot_top5_accuracy_curves('./logs/plot_logs')
     plot_time_per_batch_curves('./logs/plot_logs')
+    plot_time_breakdown('./logs/plot_logs')

64dceb561fc6df4c05442d7a6bd8fbd832f686d2
vineeths96 <vinsree1996@gmail.com>
2021-03-12 15:59:22 +0530

    Socket timings

--- socket_test_client.py
+++ socket_test_client.py
@@ -1,6 +1,6 @@
 import time
 import torch
-from socket_com import ClientTCP
+from socket_com import ClientTCP, ClientUDP
 
 
 # REPS = 10
@@ -19,14 +19,27 @@ from socket_com import ClientTCP
 #     # print("Total: ", time_sum)
 #     print(f"MSG: [{msg}] Avg TIme: {time_sum / REPS}")
 
-client = ClientTCP(SERVER="10.217.16.13")
+REPS = 10
+MSG_SIZES = [1, 5, 10, 50, 100, 500, 1000, 5e3, 1e4, 5e4, 1e5, 5e5, 1e6, 5e6, 1e7, 25e6]
+client = ClientUDP(SERVER="10.217.16.13")
 
-start = time.time()
-client.send(torch.arange(msg).to(torch.float32))
-time_elapsed = time.time() - start
-print(time_elapsed)
+for msg in MSG_SIZES:
+    time_sum = 0
+    for i in range(REPS):
+        start = time.time()
 
-# print("Total: ", time_sum)
-print(f"MSG: [{msg}] Avg TIme: {time_sum / REPS}")
+        message = torch.arange(msg)
+        for i in range(int(msg // 250) + 1):
+            client.send(message[i * 250: (i+1) * 250].to(torch.float32))
+
+            # import time
+            # time.sleep(1)
+
+        time_elapsed = time.time() - start
+        time_sum += time_elapsed
+        # print(time_elapsed)
+
+    print("Total: ", time_sum)
+    print(f"MSG: [{msg}] Avg TIme: {time_sum / REPS}")
 
 client.send(torch.tensor(float("inf")))
--- socket_test_server.py
+++ socket_test_server.py
@@ -1,4 +1,4 @@
-from socket_com import ServerTCP
+from socket_com import ServerTCP, ServerUDP
 
-server = ServerTCP(SERVER="10.217.16.13")
+server = ServerUDP(SERVER="10.217.16.13")
 server.start()

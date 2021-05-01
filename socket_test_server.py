7636d2e903c34080b17111162def435248510a66
vineeths96 <vinsree1996@gmail.com>
2021-05-01 10:20:21 +0530

    Update code

--- socket_test_server.py
+++
@@ -1,7 +0,0 @@
-from socket_com import ServerTCP, ServerUDP
-
-# server = ServerTCP(SERVER="10.216.18.179", MSG_SIZE=100000)
-server = ServerUDP(SERVER="10.216.18.179", MSG_SIZE=10000)
-
-
-server.start()

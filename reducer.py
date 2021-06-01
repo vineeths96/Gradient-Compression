2ce3c49467aa180cfb2537c35b8f37c7996094af
vineeths96 <vinsree1996@gmail.com>
2021-06-01 19:19:41 +0530

    Minor edit

--- reducer.py
+++ reducer.py
@@ -1953,13 +1953,12 @@ class RankKReducer(Reducer):
     def orthogonalize(self, matrix, eps=torch.tensor(1e-8)):
         n, m = matrix.shape
         for i in range(m):
-            # Normalize the i'th column
             col = matrix[:, i: i + 1]
             col /= torch.sqrt(torch.sum(col ** 2)) + eps
-            # Project it on the rest and remove it
+
             if i + 1 < m:
                 rest = matrix[:, i + 1:]
-                # rest -= torch.matmul(col.t(), rest) * col
+
                 rest -= torch.sum(col * rest, dim=0) * col
 
     def n_bits(self, tensor):
